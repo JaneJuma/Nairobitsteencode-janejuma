0131493759458a6e653137ef2e2341023aebf1f3
JaneJuma <jaynejumah@gmail.com>
2018-05-19 12:52:56 +0300

    add calculations

---
+++ tests.py
@@ -0,0 +1,11 @@
+num1 = 5
+num2 = 9
+num3 = 15
+
+calc = {}
+sum = num1 + num2
+product = num2 * num3
+difference = num3 - num1
+calc['addition'] = sum
+calc['product'] = product
+calc['subtraction'] = difference
